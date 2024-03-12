bbe2a0234d82515320be5e515b2a0faf48a99010
thomenz <thiagomenzinger@outlook.com>
2024-03-11 23:33:34 -0400

    responsiveness tweaks

--- pages/index.vue
+++ pages/index.vue
@@ -116,73 +116,72 @@ const habilidades = [
 const about = ref(false)
 const skills = ref(false)
 const portifolio = ref(false)
-// change the value of each ref above to true after 500 milliseconds
+
 setTimeout(() => {
   about.value = true
 }, 500)
 setTimeout(() => {
-  skills.value = true
+  portifolio.value = true
 }, 1000)
 setTimeout(() => {
-  portifolio.value = true
+  skills.value = true
 }, 1500)
 </script>
 
 <template>
-  <div class="flex flex-col min-h-[calc(100vh)] justify-between px-7 py-2 m-auto max-w-[1280px]">
+  <div class="flex flex-col min-h-[calc(100vh)] px-7 py-2 m-auto max-w-[1280px]">
     <Menu />
-    <main class="grid sm:grid-cols-2 grid-cols-1 space-y-10 py-7 items-center">
-      <article class="relative col-span-1 flex flex-col md:mx-2 lg:mx-10 sm:p-0 space-y-7">
-        <Transition name="slide-right"> 
-        <UChip  v-if="portifolio" class="absolute sm:right-3 right-0 top-10" position="top-left" :text="$t('soon')" color="yellow" size="2xl">
-        <UButton  color="gray" disabled variant="outline"> portifolio</UButton>
-      </UChip>
-    </Transition>
-        <h1 class="text-4xl whitespace-pre-line leading-tight sm:text-6xl font-bold">{{ $t('welcome') }}</h1>
-        <Transition name="slide-right">
-        <div v-if="about"
-          class="flex w-full space-y-6 rounded-lg hover:scale-105 transition-all p-5 ease-in-out duration-500 bg-gradient-to-br dark:from-[#47fab223] from-white to-transparent flex-col">
-          <div class="flex flex-row items-center space-x-6">
-            <div class="flex flex-col text-lg dark:text-gray-200">
-              <h4 class="font-bold"> {{ $t('formation') }}</h4>
-            </div>
-            <div class="bg-white rounded p-1">
-              <NuxtLink aria-label="To homepage" :to="'https://crt01.gov.br/'" target="_blank">
-                <NuxtImg class="cursor-pointer" fit="cover" width="90" src="/logocft.webp" alt="CFT Logo" />
-              </NuxtLink>
-            </div>
+    <main class="grid grid-cols-1 md:grid-cols-2 justify-center sm:flex-row gap-10 grow">
+      <article class="flex flex-col mt-10 md:self-center col-span-1 md:mx-2 lg:mx-10 sm:p-0 space-y-7">
+        <div class="relative">
+          <Transition name="slide-right">
+            <div v-if="portifolio" class="absolute right-0 top-3 sm:top-2">
+            <UChip position="top-left" :text="$t('soon')" color="yellow" size="2xl">
+              <UButton color="gray" disabled variant="outline"> portifolio</UButton>
+            </UChip>
           </div>
-          <div class="flex flex-col dark:text-gray-300">
-            <h4 class="font-bold text-lg"> {{ $t('superior') }}</h4>
-            <p> Unicesumar - 2020/2024</p>
-          </div>
+          </Transition>
+          <h1 class="text-4xl whitespace-pre-line leading-tight md:text-3xl lg:text-5xl font-bold">{{ $t('welcome') }}</h1>
         </div>
-      </Transition>
-      </article>
-      <article class="col-span-1 flex flex-col md:mx-2 lg:mx-10 sm:p-0 space-y-7">
         <Transition name="slide-right">
-        <div v-if="skills"
-          class="flex w-full space-y-6 rounded-lg hover:scale-105 transition-all p-5 ease-in-out duration-500 bg-gradient-to-br dark:from-[#4797fa37] from-white to-transparent flex-col">
-          <div class="flex flex-col text-lg dark:text-gray-300">
-            <h4 class="font-bold"> {{ $t('skills') }}</h4>
+          <div v-if="about"
+            class="flex w-full space-y-6 rounded-lg hover:scale-105 transition-all p-5 ease-in-out duration-500 bg-gradient-to-br dark:from-[#47fab223] from-white to-transparent flex-col">
+            <div class="flex flex-row gap-3 items-center">
+              <div class="flex flex-col text-lg dark:text-gray-200">
+                <h4 class="font-bold"> {{ $t('formation') }}</h4>
+              </div>
+              <NuxtImg fit="cover" sizes="100px" src="/logocft.webp" alt="CFT Logo" />
+            </div>
+            <div class="flex flex-col dark:text-gray-300">
+              <h4 class="font-bold text-lg"> {{ $t('superior') }}</h4>
+              <p> Unicesumar - 2020/2024</p>
+            </div>
           </div>
-          <div class="grid grid-cols-2 sm:grid-cols-3 gap-5">
-            <div v-for=" habilidade in habilidades" :key="habilidade.name"
-              class="col-span-1 md:hover:scale-125 hover:scale-100 md:hover:ml-4 transition-all ease-in-out duration-300 flex flex-row items-center space-x-3">
-              <UIcon class="text-4xl" :name="habilidade.icon" />
-              <div class="flex flex-col -mt-1 min-w-20 space-y-1">
-                <p class="text-sm">{{ habilidade.name }}</p>
-                <UMeter :value="habilidade.value" />
+        </Transition>
+      </article>
+      <article class="col-span-1 flex flex-col md:self-center md:mx-2 lg:mx-10 sm:p-0 space-y-7">
+        <Transition name="slide-right">
+          <div v-if="skills"
+            class="flex w-full space-y-6 rounded-lg hover:scale-105 transition-all p-5 ease-in-out duration-500 bg-gradient-to-br dark:from-[#4797fa37] from-white to-transparent flex-col">
+            <div class="flex flex-col text-lg dark:text-gray-300">
+              <h4 class="font-bold"> {{ $t('skills') }}</h4>
+            </div>
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-5">
+              <div v-for=" habilidade in habilidades" :key="habilidade.name"
+                class="col-span-1 md:hover:scale-110 hover:scale-100 transition-all ease-in-out duration-300 flex flex-row items-center space-x-3">
+                <UIcon class="text-4xl md:text-xl lg:text-4xl" :name="habilidade.icon" />
+                <div class="flex flex-col -mt-1 min-w-16 md:min-w-11 lg:min-w-16 space-y-1">
+                  <p class="text-sm">{{ habilidade.name }}</p>
+                  <UMeter :value="habilidade.value" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Transition>
+        </Transition>
       </article>
     </main>
-    <footer class="flex justify-end text-sm mb-5 gap-2 w-full">
+    <footer class="flex justify-end text-sm mb-3 mt-10 gap-2 w-full">
       <span class="text-gray-500">2023 - PRESENT &COPY Thiago Menzinger</span>
-      <span class="dark:text-gray-700 text-gray-400">Beta 1.0</span>
     </footer>
   </div>
 </template>
@@ -194,18 +193,22 @@ setTimeout(() => {
 .slide-right-leave-active {
   transition: all 0.3s;
 }
+
 .slide-left-enter-from {
   opacity: 0;
   transform: translate(50px, 0);
 }
+
 .slide-left-leave-to {
   opacity: 0;
   transform: translate(-50px, 0);
 }
+
 .slide-right-enter-from {
   opacity: 0;
   transform: translate(-50px, 0);
 }
+
 .slide-right-leave-to {
   opacity: 0;
   transform: translate(50px, 0);
