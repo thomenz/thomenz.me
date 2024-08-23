ba9b493fe08ee3cc5571b5a59f628cd384e7ef83
thomenz <thiagomenzinger@outlook.com>
2024-08-23 00:51:05 -0400

    Adicionada Rifa da Cynthya

--- pages/rifa_da_cynthya.vue
+++ pages/rifa_da_cynthya.vue
@@ -11,14 +11,27 @@ useSeoMeta({
   description: "Rifa da Cynthya",
 })
 
-const availableNumbers = ref<number[]>
+const notAvailableNumbers = ref<any>([])
+const availableNumbers = computed(() => {
+  return Array.from({ length: 300 }, (_, i) => i + 1).filter((n) => !notAvailableNumbers.value.includes(n))
+})
 
 const getAvailableNumbers = async () => {
   const response = await fetch('https://meta-ai-worker.thiagomenzinger.workers.dev/rifas',)
-  console.log(response)
+  const data = await response.json()
+  // for each entry in data that contains numeros: "1,2,3,4,5" split it and map to a number array
+  notAvailableNumbers.value = data.map((entry: { numeros: string }) => entry.numeros.split(',').map(Number)).flat()
 }
 
+await getAvailableNumbers()
+
 const selected = ref<number[]>([])
+
+const sendWhatsappMessage = () => {
+  const numbers = selected.value.join(',')
+  const message = `Olá, gostaria de comprar os números ${numbers} da rifa da Cynthya!`
+  window.open(`https://api.whatsapp.com/send?phone=556784132198&text=${message}`)
+}
 
 const selectNumber = (n: number) => {
   if (selected.value.includes(n)) {
@@ -54,7 +67,7 @@ const value = computed(() => {
       <p>Clique nos números para selecionar!</p>
     </div>
     <div class="grid grid-cols-5 md:grid-cols-8 lg:grid-cols-12 gap-4 mt-3 p-4 mb-16">
-      <div v-for="n in 10" class="flex flex-row items-center">
+      <div v-for="n in availableNumbers" class="flex flex-row items-center">
         <div @click="selectNumber(n)"
           :class="[isSelected(n), 'flex flex-col text-white font-bold ml-2 bg-pink-300 w-11 h-11 text-center justify-center text-bold rounded-full p-1 px-2']">
           {{ n }}</div>
@@ -68,7 +81,7 @@ const value = computed(() => {
           {{ value + `,00` }} R$
         </div>
         <div>
-          <UButton :disabled="selected.length >= 1 ? false : true" @click="getAvailableNumbers()" size="xl" color="pink">
+          <UButton :disabled="selected.length >= 1 ? false : true" @click="sendWhatsappMessage()" size="xl" color="pink">
             Quero esses! </UButton>
         </div>
       </div>
